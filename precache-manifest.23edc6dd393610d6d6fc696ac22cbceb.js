9f3f3be871043c9f8932bc3ce71788ba64513944
Daniil Barabash <daniil.barabash@jumpshot.com>
2019-01-21 16:23:43 +0100

    initial

---
+++ precache-manifest.23edc6dd393610d6d6fc696ac22cbceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eaaf3582bdc0376efab",
+    "url": "/static/js/main.9eaaf358.chunk.js"
+  },
+  {
+    "revision": "a6da914c8c43a4b5346b",
+    "url": "/static/js/1.a6da914c.chunk.js"
+  },
+  {
+    "revision": "9eaaf3582bdc0376efab",
+    "url": "/static/css/main.b44e373e.chunk.css"
+  },
+  {
+    "revision": "41cc9c56bcb24d9e81ad3bcdcb9ff7cb",
+    "url": "/index.html"
+  }
+];
